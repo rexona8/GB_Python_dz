4d36d1193a865a1726bbc1bb745ff0da646b86c8
Nikita Vasilchenko <rexona8@yandex.ru>
2023-07-09 08:45:14 +0300

    fixed: replaced function name

--- seminar5_dz/task28.py
+++ seminar5_dz/task28.py
@@ -4,12 +4,12 @@
 # 2 2
 # 4
 
-def sum(num1, num2):
+def sum_nums(num1, num2):
     if num1 == 0:
         return num2
-    return sum(num1 - 1, num2 + 1)
+    return sum_nums(num1 - 1, num2 + 1)
 
 a = int(input("Введите целое, не отрицательное число a: "))
 b = int(input("Введите целое, не отрицательное число b: "))
 
-print(sum(a, b))
+print(sum_nums(a, b))
